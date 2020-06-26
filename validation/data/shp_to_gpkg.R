168ce3a9d10b65235769037d2c2bc2613fc53ae2
Jeffery Sauer <jeffery.sauer@mail.mcgill.ca>
2020-06-26 15:53:20 -0500

    Adding new validation file

--- validation/data/shp_to_gpkg.R
+++ validation/data/shp_to_gpkg.R
@@ -1,6 +1,6 @@
 library(spdep)
 library(rgdal)
 temp <- st_read("C:/Users/jeffe/Dropbox/GSOC2020/validation/data/baltimore/baltimore_housing.shp")
-outname <- "C:/Users/jeffe/Dropbox/GSOC2020/validation/data/baltimore/baltimore_housing.gpkg"
+outname <- "C:/Users/jeffe/Dropbox/GSOC2020/validation/data/commpop.gpkg"
 temp_sp <- as_Spatial(temp)
-writeOGR(temp_sp, dsn = outname, layer = "baltimore", driver = "GPKG")
+writeOGR(temp_sp, dsn = outname, layer = "commpop", driver = "GPKG")
