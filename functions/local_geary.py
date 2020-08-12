33ad16190d18a727a981df3468fa0a5a6d4c78a1
jeffcsauer <jeffery.sauer@mail.mcgill.ca>
2020-08-12 17:44:51 -0500

    Fixing Local Geary values of interest, working on LOSH conditional randomization

--- functions/local_geary.py
+++ functions/local_geary.py
@@ -13,18 +13,30 @@ from esda.crand import (
 
 
 PERMUTATIONS = 999
+SIG = 0.05
 
 
 class Local_Geary(BaseEstimator):
     """Local Geary - Univariate"""
 
-    def __init__(self, connectivity=None, permutations=PERMUTATIONS, n_jobs=1,
-                 keep_simulations=True, seed=None):
+    def __init__(self, connectivity=None, autocorr=False, sig=SIG,
+                 permutations=PERMUTATIONS, n_jobs=1, keep_simulations=True,
+                 seed=None):
         """
         connectivity     : scipy.sparse matrix object
                            the connectivity structure describing
                            the relationships between observed units.
                            Need not be row-standardized.
+        autocorr         : boolean
+                           (default=False)
+                           If True use, indicate if an observation 
+                           belongs to an outlier, cluster, undefined,
+                           or non-significant group. 1 = outlier, 
+                           2 = cluster, 3 = undefined, 4 = non-significant.
+        sig              : float
+                           (default=0.05)
+                           Default significance threshold used for 
+                           creation of autocorr groups.
         permutations     : int
                            number of random permutations for calculation
                            of pseudo p_values
@@ -54,6 +66,8 @@ class Local_Geary(BaseEstimator):
         """
 
         self.connectivity = connectivity
+        self.autocorr = autocorr
+        self.sig = sig
         self.permutations = permutations
         self.n_jobs = n_jobs
         self.keep_simulations = keep_simulations
@@ -104,6 +118,23 @@ class Local_Geary(BaseEstimator):
                 n_jobs=n_jobs,
                 stat_func=_local_geary
             )
+            
+        if self.autocorr:    
+            Eij_mean = np.mean(self.localG)
+            x_mean = np.mean(x)
+            # Create empty vector where default is undefined.
+            self.q = np.ones(len(x))*3
+            # Outliers
+            self.q[(self.localG < Eij_mean) & \
+                   (y > y_mean) & \
+                   (self.p_sim<=self.sig)] = 1
+            # Clusters
+            self.q[(self.localG < Eij_mean) & \
+                   (y < y_mean) & \
+                   (self.p_sim<=self.sig)] = 2
+            # Undefined: default value in empty vector
+            # Non-significant
+            self.q[self.p_sim > self.sig] = 4
 
         del (self.keep_simulations, self.n_jobs,
              self.permutations, self.seed, self.rlocalG,
